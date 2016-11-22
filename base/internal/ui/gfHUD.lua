dd04456e948e65663d5df5182450daa9fbbfcbb1
Marvin <github@gfield.de>
2016-11-22 11:06:09 +0100

    Added basic shadows to boxes

--- base/internal/ui/gfHUD.lua
+++ base/internal/ui/gfHUD.lua
@@ -7,6 +7,8 @@ GF_FONT_SIZE_SMALL = 48;
 GF_FONT_SIZE_MEDIUM = 80;
 GF_FONT_SIZE_BIG = 128;
 
+GF_CORNER_RADIUS = 2;
+
 GF_COLORS = {
 	orange = Color(224, 151, 50),
 	green = Color(79, 224, 59),
@@ -60,14 +62,35 @@ GF_WEAPON_ICON_WIDTH = {
 function drawSquare (x, y, w, h, color)
 	--x = x - (w/2);
 	y = y - (h/2);
+
+	-- shadow
+
+	nvgBeginPath();
+	--nvgRect(x, y, w + 20, h + 30);
+	nvgRoundedRect(x-12, y-12, w+24, h+24, GF_CORNER_RADIUS);
+	nvgPathWinding(NVG_HOLE);
+	nvgFillBoxGradient(x, y, w, h, GF_CORNER_RADIUS, 24, Color(0, 0, 0, 51), Color(0, 0, 0, 0));
+	nvgFill();
+	-- shadow top
+
+
+	-- shadow bottom
+	nvgBeginPath();
+	--nvgRect(x, y, w + 20, h + 30);
+	nvgRoundedRect(x-2, y+2, w+4, h+4, GF_CORNER_RADIUS);
+	nvgPathWinding(NVG_HOLE);
+	nvgFillBoxGradient(x, y, w, h, GF_CORNER_RADIUS, 24, Color(0, 0, 0, 84), Color(0, 0, 0, 0));
+	nvgFill();
+
+	-- box
 	nvgBeginPath();
 	nvgFillColor(color);
-	nvgRoundedRect(x, y, w, h, 2);
+	nvgRoundedRect(x, y, w, h, GF_CORNER_RADIUS);
 	nvgFill();
-	--nvgRestore();
 end
 
 function drawShadowText (x, y, size, align, color, text)
+	nvgBeginPath();
 	nvgFontFace(GF_FONT);
 	nvgFontSize(size);
 	nvgTextAlign(align, NVG_ALIGN_MIDDLE);
