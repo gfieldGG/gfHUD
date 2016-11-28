8f91966e60c971a0371f38edbd8f81dcd72008e3
Marvin <github@gfield.de>
2016-11-28 15:44:14 +0100

    Basic buff functionality

--- base/internal/ui/widgets/gfBuffs.lua
+++ base/internal/ui/widgets/gfBuffs.lua
@@ -16,8 +16,43 @@ end
 
 --
 
+local function drawBuff (x, y, color, icon, percent)
+	drawBox(x, y, 24, 24, color);
+end
+
 function gfBuffs:draw()
 	if not shouldShowHUD() then return end;
-	if isRaceMode() then return end;
 
+	local player = getPlayer();
+	if player == nil then return end;
+
+	local x = -12;
+	local y = -241;
+
+	-- carnage
+	if player.carnageTimer > 0 then
+		drawBuff(x, y, Color(224,151,50), "internal/ui/icons/carnage");
+		y = y - 32;
+	end
+
+	-- resist
+	if player.resistTimer > 0 then
+		drawBuff(x, y, Color(255,120,128), "internal/ui/icons/resist");
+		y = y - 32;
+	end
+
+	-- mega
+	if player.hasMega then
+		drawBuff(x, y, Color(49,68,224), "internal/ui/icons/health");
+		y = y - 32;
+	end
+
+	-- flag
+	if player.hasFlag then
+		local teamFlagHolding = (player.team == 1) and 2 or 1; -- (other team flag)
+		local icon = "internal/ui/icons/CTFflag";
+		local iconCol = teamColors[teamFlagHolding];
+		drawBuff(x, y, iconCol, icon);
+		y = y - 32;
+	end
 end
